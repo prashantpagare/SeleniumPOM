d3183ae37718a91d8335f92f3ccf9c6a715d130d
Prashant Pagare <ppagare.pagare@gmail.com>
2022-03-14 13:13:49 +0530

    chanhes done in CarBase

--- Pages/CarBase.py
+++ Pages/CarBase.py
@@ -12,7 +12,7 @@ class CarBase:
     def getCarNamesAndPrices(self):
         carNames = self.driver.find_elements_by_xpath(configReader.readConfig("locators", "CarNames_XPATH"))
         carPrices = self.driver.find_elements_by_xpath(configReader.readConfig("locators", "CarPrices_XPATH"))
-
+        # adding comments
         for i in range(1,len(carPrices)):
             print((carNames[i].text + "Prices are :" + carPrices[i].text).encode('utf8'))
 
